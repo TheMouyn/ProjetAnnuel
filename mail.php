cb20b8d7e484e0596ea8a948916ab99ccb7b318f
Maxime <monillon@outlook.fr>
2021-01-16 12:03:02 +0100

    Suppression fichier Maxime test

--- mail.php
+++
@@ -1,8 +0,0 @@
-<?php
-$to = 'monillon@outlook.fr';
-$objet = 'Ceci est un test';
-$message = 'Ceci est un super message de folie pour tester les mail et voir si cela fonctionne correctement';
-
-
-mail($to, $objet, $message);
-
