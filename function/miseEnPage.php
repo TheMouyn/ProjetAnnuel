66da1207870b8ba105210107318dabd152a7ebb1
Maxime <monillon@outlook.fr>
2021-02-07 15:14:30 +0100

    Ajout d'une fonction de mise en page d'un message erreur ou success

--- function/miseEnPage.php
+++ function/miseEnPage.php
@@ -1,4 +1,15 @@
 <?php
+// permet de changer le format de la date sotcke en bdd en format français
 function enDate($date):string{
    return (date('d/m/Y', strtotime($date)));
+}
+
+// Fonction pour afficher un message d'erreur ou de success
+function message($message, $type):string{
+   return <<<HTML
+    <div style="width: 100%; display: flex; justify-content: center;">
+        <p class="$type"> $message </p>
+    </div>
+HTML;
+
 }
