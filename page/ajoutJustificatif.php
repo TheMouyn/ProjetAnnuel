74b52c1f903d58e4b2df9843f0f5f2d7882a4537
Maxime <monillon@outlook.fr>
2021-02-19 12:16:02 +0100

    Ajout de la page pour upload un justificatif professionnel

---
+++ page/ajoutJustificatif.php
@@ -0,0 +1,18 @@
+<?php
+$titreOnglet = "Bactépédia - Ajout justificatif";
+$titrePage = "Page d'Accueil - Ajout du justificatif professionnel";
+require_once '../elements/header.php';
+require_once '../elements/nav.php';
+require_once '../function/function.php';
+
+//TODO: création du formulaire pour ajouter un justificatif
+?>
+<div class="contenu">
+
+
+
+</div>
+
+<?php
+require_once '../elements/footer.php';
+?>
