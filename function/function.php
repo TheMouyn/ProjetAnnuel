8b1432e07f75dbc87ce49d738ab353f3493b7364
Maxime <monillon@outlook.fr>
2021-03-09 09:32:59 +0100

    Modification commentaires fonction ajout consultation

--- function/function.php
+++ function/function.php
@@ -354,7 +354,7 @@ function updatePassword($password, $idUser){
 
 
 function ajoutConsultation($idBac){
-    // permet de modifier le mot de passe de l'utilisateur avec son id
+    // permet d'ajouter un au nombre de fois où la bactérie est consultée
     $bdd = connect();
     $query = $bdd->prepare('UPDATE bcp__bacterie SET nbConsultation_bacterie = nbConsultation_bacterie+1 WHERE id_bacterie = :idBac;');
     $query->execute([
